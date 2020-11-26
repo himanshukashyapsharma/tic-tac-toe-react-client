c2fd3ba295e51dee73388286d81938b9e06d6693
himanshukashyapsharma <himanshukashyapsharma@gmail.com>
2020-11-26 12:59:28 +0530

    till here i used 2d array

--- src/Components/game.js
+++ src/Components/game.js
@@ -13,9 +13,10 @@ function game({location}) {
 
     const [Board,setBoard] = new useState([[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']])
     const [IsPlayed,setIsPlayed] = new useState([[false,false,false],[false,false,false],[false,false,false]])
-    const [RoomData,setRoomData] = new useState() // use this to implement user names
+    // const [RoomData,setRoomData] = new useState() // use this to implement user names
+    const [UserWon, setUserWon] = new useState()
+    const [IsTurn,setIsTurn] = new useState(true)
     const ENDPOINT = "http://localhost:5000/"
-    console.log(Board)
 
     new useEffect(()=>{
         const {name,room} = queryString.parse(location.search)
@@ -31,67 +32,102 @@ function game({location}) {
     },[ENDPOINT,location.search])
 
     new useEffect(()=>{
-
-        //listens for and updates messages array when message is recieved from server side
-
-        //.once prevents from creating multiple listeners on server side
-        //using socket.on will lead to creating multiple listeners on server side whenever client side is reloaded
-        //or if it has multiple clients
         
-        // recieves message from server
-        socket.once("2playturn",({i,j,value})=>{   
+        // recieves position of second players move from server
+        socket.once("2playturn",({i,j,newBooleanArray})=>{   
             // set board value according to the values and position recieved by other user and prevent them form being clickable too.
             let newBoard = Board
             let newArray = Board[i]
             newArray[j] = 'X'
+            newBoard[i] = newArray
             setBoard([...newBoard])
+            setIsPlayed([...newBooleanArray])
+            setIsTurn(true)
         })
 
         // reloads whenever messages array is updated
-    },[Board])
+    },[Board,IsTurn,IsPlayed])
 
-    // new useEffect(() => {
-    //     socket.once("roomData",({room,users}) => {
-    //         if(users.length == 1) setSymbol('O')
-    //         if(users.length == 2) setSymbol('X')
-    //     })
-    // },[])
+    new useEffect(() => {
+        for(let i=0;i<3;i++){
+            //checking horizontal 
+            if (Board[i][0] !== ' ' && Board[i][1] !== ' ' && Board[i][2] !== ' '){
+                if(Board[i][0] === Board[i][1] && Board[i][1] === Board[i][2]){
+                    setUserWon(Board[i][0])
+                    break
+                } 
+            }
+            //checking vertical rows
+            if (Board[0][i] !== ' ' && Board[1][i] !== ' ' && Board[2][i] !== ' '){
+                if(Board[0][i] === Board[1][i] && Board[1][i] === Board[2][i]){
+                    setUserWon(Board[0][i])
+                    break
+                } 
+            }
+        }
+        //checks diagonals
+        if (Board[0][0] !== ' ' && Board[1][1] !== ' ' && Board[2][2] !== ' '){
+            if(Board[0][0] === Board[1][1] && Board[1][1] === Board[2][2]){
+                setUserWon(Board[0][0])
+                
+            } 
+        }
+        if (Board[0][2] !== ' ' && Board[1][1] !== ' ' && Board[2][0] !== ' '){
+            if(Board[0][2] === Board[1][1] && Board[1][1] === Board[2][0]){
+                setUserWon(Board[0][2])
+                
+            } 
+        }
+
+        // write draw login here <<<<<<<<<<<<<<<<<<<<<<<<<
+
+        //if user won 
+        if(UserWon){
+            setBoard([[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']])
+            setIsPlayed([[false,false,false],[false,false,false],[false,false,false]])
+            setUserWon()
+        }
+
+    },[Board,UserWon])
 
     const playTurn = (event) => {
         event.preventDefault()
         let i = event.target.getAttribute('i')
         let j = event.target.getAttribute('j')
-        console.log(i,j)
-        let value = event.target.value
-        //sends message only if message variable is not null
-        /////////////////"playTurn" was previously message 
-            socket.emit("1playTurn",{i,j,value},()=> {
-                let arrayRow = [...Board[i]]
-                let BooleanRow = [...IsPlayed[i]]
-                BooleanRow[j] = true
-                arrayRow[j] = 'O'
-                let newArrayBoard = Board
-                let newBooleanArray = IsPlayed
-                newBooleanArray[i] = BooleanRow
-                newArrayBoard[i] = arrayRow
-                setBoard(newArrayBoard)
-            })
-        console.log(Board)
+        
+        // to update array on player 1 side
+        let arrayRow = [...Board[i]]
+        let booleanRow = [...IsPlayed[i]]
+        booleanRow[j] = true
+        arrayRow[j] = 'O'
+        let newArrayBoard = Board
+        let newBooleanArray = IsPlayed
+        newBooleanArray[i] = booleanRow
+        newArrayBoard[i] = arrayRow
+        setBoard(newArrayBoard)
+        setIsPlayed(newBooleanArray)
+        setIsTurn(false)
+        //emits 1players moves to the server
+        socket.emit("1playTurn",{i,j,newBooleanArray})
     }
 
 
     return (
         <div>
             <div className="board">
-                <button onClick={playTurn} i={0} j={0} className="board-box" value={Board[0][0]}>{Board[0][0]}</button>
-                <button onClick={playTurn} i={0} j={1} className="board-box" value={Board[0][1]}>{Board[0][1]}</button>
-                <button onClick={playTurn} i={0} j={2} className="board-box" value={Board[0][2]}>{Board[0][2]}</button>
-                <button onClick={playTurn} i={1} j={0} className="board-box" value={Board[1][0]}>{Board[1][0]}</button>
-                <button onClick={playTurn} i={1} j={1} className="board-box" value={Board[1][1]}>{Board[1][1]}</button>
-                <button onClick={playTurn} i={1} j={2} className="board-box" value={Board[1][2]}>{Board[1][2]}</button>
-                <button onClick={playTurn} i={2} j={0} className="board-box" value={Board[2][0]}>{Board[2][0]}</button>
-                <button onClick={playTurn} i={2} j={1} className="board-box" value={Board[2][1]}>{Board[2][1]}</button>
-                <button onClick={playTurn} i={2} j={2} className="board-box" value={Board[2][2]}>{Board[2][2]}</button>
+                <button onClick={playTurn} i={0} j={0} disabled={IsPlayed[0][0] && IsTurn} className="board-box" value={Board[0][0]}>{Board[0][0]}</button>
+                <button onClick={playTurn} i={0} j={1} disabled={IsPlayed[0][1] && IsTurn} className="board-box" value={Board[0][1]}>{Board[0][1]}</button>
+                <button onClick={playTurn} i={0} j={2} disabled={IsPlayed[0][2] && IsTurn} className="board-box" value={Board[0][2]}>{Board[0][2]}</button>
+                <button onClick={playTurn} i={1} j={0} disabled={IsPlayed[1][0] && IsTurn} className="board-box" value={Board[1][0]}>{Board[1][0]}</button>
+                <button onClick={playTurn} i={1} j={1} disabled={IsPlayed[1][1] && IsTurn} className="board-box" value={Board[1][1]}>{Board[1][1]}</button>
+                <button onClick={playTurn} i={1} j={2} disabled={IsPlayed[1][2] && IsTurn} className="board-box" value={Board[1][2]}>{Board[1][2]}</button>
+                <button onClick={playTurn} i={2} j={0} disabled={IsPlayed[2][0] && IsTurn} className="board-box" value={Board[2][0]}>{Board[2][0]}</button>
+                <button onClick={playTurn} i={2} j={1} disabled={IsPlayed[2][1] && IsTurn} className="board-box" value={Board[2][1]}>{Board[2][1]}</button>
+                <button onClick={playTurn} i={2} j={2} disabled={IsPlayed[2][2] && IsTurn} className="board-box" value={Board[2][2]}>{Board[2][2]}</button>
+            </div>
+
+            <div>
+                <h2 id="user-won-message">{UserWon && `${UserWon} has won the game`}</h2>
             </div>
         </div>
     )
